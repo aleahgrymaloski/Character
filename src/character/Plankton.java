e74f274235c71e3ebc053a592b9ebf9d18b49d49
aleahgrym <aleah.grymaloski@brentwood.ca>
2015-11-03 10:11:23 -0800

    Final Plankton Drawing

--- src/character/Plankton.java
+++ src/character/Plankton.java
@@ -56,10 +56,68 @@ public class Plankton {
         graphics.setColor(GREEN_BODY);
         graphics.fillOval(x + (width * 50 / 100), y + (height * 70 /100), width * 50 / 100, height * 5 / 100);
         
-        
+        //mouth
         graphics.setColor(Color.RED);
-        graphics.fillOval(x + (width * 40 / 100), y + (height * 70 /100), width * 25 / 100, height * 5 / 100);
-//        
+        graphics.fillArc(x + (width * 30 / 100), y + (height * 60 /100), width * 40 / 100, height * 20 / 100, 170, 200);
+//      
+        graphics.setColor(Color.WHITE);
+        graphics.fillArc(x + (width * 30 / 100), y + (height * 63 / 100), width * 40 / 100, height * 10 / 100, 170, 190);
+        graphics.fillOval(x + (width * 35 / 100), y + (height * 75 / 100), width *   28 / 100, height * 5 / 100);
+
+        //right antennae
+        int[] xValues = new int[7];
+        xValues[0] = x + (width * 5 /  100);
+        xValues[1] = x + (width * 10 / 100);
+        xValues[2] = x + (width * 15 / 100);
+        xValues[3] = x + (width * 20 / 100);
+        xValues[4] = x + (width * 25 / 100);
+        xValues[5] = x + (width * 20 / 100);
+        xValues[6] = x + (width * 18 / 100);
+
+        int[] yValues = new int[7];
+        yValues[0] = y + (height * 5 /  100);
+        yValues[1] = y + (height * 30 / 100);
+        yValues[2] = y + (height * 35 / 100);
+        yValues[3] = y + (height * 53 / 100);
+        yValues[4] = y + (height * 53 / 100);
+        yValues[5] = y + (height * 35 / 100);
+        yValues[6] = y + (height * 30 / 100);
+        
+        graphics.setColor(GREEN_BODY);
+        graphics.fillPolygon(xValues, yValues, xValues.length);
+        
+        //left antennae
+        xValues[0] = x + (width * 95 /  100);
+        xValues[1] = x + (width * 90 / 100);
+        xValues[2] = x + (width * 85 / 100);
+        xValues[3] = x + (width * 80 / 100);
+        xValues[4] = x + (width * 75 / 100);
+        xValues[5] = x + (width * 80 / 100);
+        xValues[6] = x + (width * 82 / 100);
+
+        yValues[0] = y + (height * 5 /  100);
+        yValues[1] = y + (height * 30 / 100);
+        yValues[2] = y + (height * 35 / 100);
+        yValues[3] = y + (height * 53 / 100);
+        yValues[4] = y + (height * 53 / 100);
+        yValues[5] = y + (height * 35 / 100);
+        yValues[6] = y + (height * 30 / 100);
+        
+        graphics.setColor(GREEN_BODY);
+        graphics.fillPolygon(xValues, yValues, xValues.length);
+        
+        
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(x + (width * 74 / 100), y + (height * 35 / 100), width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 75 / 100), y + (height * 43 / 100), width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 75 / 100), y + (height * 25 / 100), width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 76 / 100), y + (height * 15 / 100), width * 20 / 100 , height * 1 / 100);
+
+        
+        graphics.fillRect(x + (width * 3 / 100), y +  (height * 15 / 100),  width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 5 / 100), y + (height *  25 / 100), width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 10 / 100), y + (height * 35 / 100), width * 20 / 100 , height * 1 / 100);
+        graphics.fillRect(x + (width * 10 / 100), y + (height * 43 / 100), width * 20 / 100 , height * 1 / 100);
 //        Graphics2D g2d = (Graphics2D) graphics;
 //        g2d.rotate(Math.toRadians(45));
 //        g2d.fillOval(x + 150, y - 200, width / 2, height /2);
